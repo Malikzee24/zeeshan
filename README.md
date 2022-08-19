bbdbe079c91faba475ed7ce1677cd7c83a28f84e
Malikzee24 <66636733+Malikzee24@users.noreply.github.com>
2022-08-19 10:10:11 +0500

    Update and rename README.md to projectofpython.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# zeeshan
-Coding Projects
---
+++ projectofpython.py
@@ -0,0 +1,10 @@
+# Project:
+Print only numaric numbers lesser then 30 and typecast int into str?
+
+#Given Data:
+
+data: [72, 32, 43, 34, 54, 65,24, 96, 78, "Intel", float, int]
+
+for numbers in data:
+       if str(numbers).isnumeric and numbers>= 30
+       print(numbers)
